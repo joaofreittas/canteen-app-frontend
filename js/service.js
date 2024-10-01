6f9a00fce454bd043cf9c0f1bf8967b42b2cb42d
joaofreittas <joao.frhb@gmail.com>
2024-10-01 09:33:27 -0300

    feat: add frontent initial project

---
+++ js/service.js
@@ -0,0 +1,22 @@
+const getAccountReport = (accountId) => {
+
+}
+
+const createAccount = (name, amount = 0) => {
+    
+}
+
+const registerPurchase = (accountId, amount) => {
+        
+}
+
+const payBalance = (accountId, amount) => {
+    
+}
+
+export default{ 
+    getAccountReport, 
+    createAccount, 
+    registerPurchase, 
+    payBalance 
+}
